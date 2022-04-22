084640865e6fdb057d6f3d8b509d390f4ee49790
VladimirLinevich <linevichv@gmail.com>
2022-04-22 16:58:49 +0300

    Проект готов в версии 1.0

--- game_v3.py
+++ game_v3.py
@@ -8,16 +8,16 @@ import numpy as np
 def smart_predict(number: int = 1) -> int:
     """Функция угадывает число, учитывая, что на каждой итерации нам известно:
     больше либо меньше загаданное число, чем наше преполагаемое
-
+    В каждой итерации уменьшаем диапазон гадания, в зависимости от того,
+    меньше загаданное число, либо больше.
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
-
     Returns:
         int: Число попыток
     """
-    count = 0
-    upper_bound = 100
-    lower_bound = 0
+    count = 0  # счетчик попыток
+    upper_bound = 100  # верхняя граница диапазона "гадания"
+    lower_bound = 0  # нижняя граница диапазона "гадания"
     while True:
         count += 1
         predict_number = np.random.randint(lower_bound, upper_bound+1)
@@ -25,19 +25,15 @@ def smart_predict(number: int = 1) -> int:
             break
         elif number < predict_number:
             upper_bound = predict_number-1
-            #predict_number = np.random.randint(lower_bound, upper_bound+1) # ((upper_bound+lower_bound)/2)
         elif number > predict_number:
             lower_bound = predict_number+1
-            #predict_number = np.random.randint(lower_bound, upper_bound+1) # int((upper_bound+lower_bound)/2)
     return count
 
 
-def score_game(smart_predict) -> int:
+def score_game(predict_func) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
-
     Args:
-        smart_predict ([type]): функция угадывания
-
+        predict_func ([type]): функция угадывания
     Returns:
         int: среднее количество попыток
     """
@@ -45,7 +41,7 @@ def score_game(smart_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000))  # загадали числа
 
     for number in random_array:
-        count_ls.append(smart_predict(number))
+        count_ls.append(predict_func(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
